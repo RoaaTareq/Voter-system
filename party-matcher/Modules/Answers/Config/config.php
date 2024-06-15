1f39a80b508cfb086f2ae6c7ace78a2947a19e82
Roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-06-15 14:36:24 +0300

    first version

--- party-matcher/Modules/Answers/Config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'name' => 'Answers'
-];
--- party-matcher/Modules/Questions/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('questions::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('questions.name') !!}
-    </p>
-@endsection
