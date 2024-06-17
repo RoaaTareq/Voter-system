99c6f793ddafb1265c6a66050a271b92d6059245
Roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-06-18 01:46:10 +0300

    UI

---
+++ resources/views/Category/category.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href="{{ asset('/css/category.css') }}"/>
+    
+</head>
+<body>
+
+<div class="d-flex m-auto justify-content-center align-items-center">
+    <div class='cards'>
+        <h1>القائمة المحلية</h1>
+  </div>
+    <div class='cards'>
+       <h1> القائمة العامة</h1>
+    </div>
+</div>
+
+   
+</body>
+</html>
